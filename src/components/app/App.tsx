628549f580b80ab914f18833bc06ea14900f9245
Valentin Vetyukov <finik001@gmail.com>
2023-02-07 20:43:13 +0300

    App приведен к виду функциональному виду компонента

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 
 import AppHeader from "../app-header/app-header";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
@@ -7,42 +7,39 @@ import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import { getBurgerIngredient } from "../../api/burger-api";
 
 import styleClass from "./App.module.css";
+import IBurderIngredient from "../../models/byrger-ingredient";
 
-class App extends Component {
-  state = {
-    data: [],
-  };
+const App = () => {
+  const [data, setData] = useState<IBurderIngredient[]>([]);
 
-  componentDidMount(): void {
+  useEffect(() => {
     setTimeout(() => {
-      this.setState({ ...this.state, data: getBurgerIngredient() });
+      setData(getBurgerIngredient());
     }, 750);
-  }
+  }, []);
 
-  render() {
-    const content =
-      this.state.data.length === 0 ? (
-        <h3
-          className={`text text_type_main-large mb-5 ${styleClass.loading_text}`}
-        >
-          Загрузка данных
-        </h3>
-      ) : (
-        <main>
-          <div className={styleClass.container}>
-            <BurgerIngredients data={this.state.data} />
-            <BurgerConstructor data={this.state.data} />
-          </div>
-        </main>
-      );
-
-    return (
-      <div className={styleClass.app}>
-        <AppHeader />
-        {content}
-      </div>
+  const content =
+    data.length === 0 ? (
+      <h3
+        className={`text text_type_main-large mb-5 ${styleClass.loading_text}`}
+      >
+        Загрузка данных
+      </h3>
+    ) : (
+      <main>
+        <div className={styleClass.container}>
+          <BurgerIngredients data={data} />
+          <BurgerConstructor data={data} />
+        </div>
+      </main>
     );
-  }
-}
+
+  return (
+    <div className={styleClass.app}>
+      <AppHeader />
+      {content}
+    </div>
+  );
+};
 
 export default App;
