066045565c794f31e518b30e6518dfb8a823ed0a
Valentin Vetyukov <finik001@gmail.com>
2023-02-17 22:35:36 +0300

    Совет по минимизации кода в API и добавления функции checkResponse

--- src/api/burger-api.ts
+++ src/api/burger-api.ts
@@ -2,12 +2,7 @@ import IRequestOrder from "../models/request-order";
 import { apiGetDataPath, apiPostSendOrderPath } from "./../helpers/SD";
 
 async function getBurgerIngredient() {
-  return await fetch(apiGetDataPath).then((res) => {
-    if (res.ok) {
-      return res.json();
-    }
-    throw new Error("ошибка получения данных");
-  });
+  return await fetch(apiGetDataPath).then(checkResponse);
 }
 
 async function fetchSendOrder(body: IRequestOrder) {
@@ -17,12 +12,15 @@ async function fetchSendOrder(body: IRequestOrder) {
       "Content-Type": "application/json;charset=utf-8",
     },
     body: JSON.stringify(body),
-  }).then((res) => {
-    if (res.ok) {
-      return res.json();
-    }
-    throw new Error("ошибка получения данных");
-  });
+  }).then(checkResponse);
+}
+
+async function checkResponse(res: Response) {
+  return res.ok
+    ? res.json()
+    : res.json().then((err) => {
+        throw new Error(`ошибка получения данных: ${err}`);
+      });
 }
 
 export { getBurgerIngredient, fetchSendOrder };
